1d09726afb52e3e96b6a73eb28918c083ae43671
ZIlko <69888540+ZiLko@users.noreply.github.com>
2024-02-09 15:42:29 -0600

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -553,7 +553,7 @@ void addButton(const char* id) {
     	auto btn = CCMenuItemSpriteExtra::create(
         	spr,
         	PlayLayer::get(),
-	nullptr;
+	nullptr
     	);
 	auto winSize = CCDirector::sharedDirector()->getWinSize();
 	label->setPosition(winSize/2 + CCPOINT_CREATE(-winSize.width/2, -winSize.height/2) + CCPOINT_CREATE(6, 30));
