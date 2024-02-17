bcc677ebb46e9decbb47f9a6a415f97c3fcc18b4
ZIlko <69888540+ZiLko@users.noreply.github.com>
2024-02-16 23:15:23 -0600

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1381,7 +1381,7 @@ class $modify(EndLevelLayer) {
 		if (Mod::get()->getSettingValue<bool>("end_button")) {
 			auto winSize = CCDirector::sharedDirector()->getWinSize();
 			CCSprite* sprite = nullptr;
-
+			sprite = CCSprite::createWithSpriteFrameName("GJ_playBtn2_001.png");
         	auto btn = CCMenuItemSpriteExtra::create(sprite,
 			this,
 			menu_selector(RecordLayer::openMenu));
@@ -1422,7 +1422,7 @@ class $modify(CCScheduler) {
 		if (recorder.state == state::off) return CCScheduler::update(dt);
 
 		if (holdV) holdCooldown++;
-		if (holdCooldown > (recorder.fps/4) * (recorder.fps/60)) {
+		if (holdCooldown > 15 * (recorder.fps/60)) {
 			if (!Mod::get()->getSettingValue<bool>("disable_frame_stepper")) {
 				if (Mod::get()->getSettingValue<bool>("frame_stepper")) stepFrame = true;
 				else {
