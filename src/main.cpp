615396443189a6c83da6132b9b91e0a1ca6364db
ZIlko <69888540+ZiLko@users.noreply.github.com>
2024-02-16 20:40:27 -0600

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -179,7 +179,7 @@ CCLabelBMFont* fpsLabel = nullptr;
 protected:
     bool setup(std::string const& value) override {
         auto winSize = cocos2d::CCDirector::sharedDirector()->getWinSize();
-		auto versionLabel = CCLabelBMFont::create("xdBot v1.4.4 - made by Zilko", "chatFont.fnt");
+		auto versionLabel = CCLabelBMFont::create("xdBot v1.4.5 - made by Zilko", "chatFont.fnt");
 		versionLabel->setOpacity(60);
 		versionLabel->setAnchorPoint(ccp(0.0f,0.5f));
 		versionLabel->setPosition(winSize/2 + ccp(-winSize.width/2, -winSize.height/2) + ccp(3, 6));
@@ -1388,7 +1388,8 @@ class $modify(EndLevelLayer) {
 
 			btn->setPosition(winSize/2 + CCPOINT_CREATE(-winSize.width/2, -winSize.height/2) + CCPOINT_CREATE(442, 61));
 
-        	this->addChild(btn);
+			auto layer = reinterpret_cast<CCLayer*>(this->getChildren()->objectAtIndex(0));
+        	layer->addChild(btn);
 		}
 	}
 
